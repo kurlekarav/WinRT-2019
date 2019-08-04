153cb300a4c757431d45a09368bfe00b145fad96
Kurlekarav <kurlekarav@gmail.com>
2019-08-04 16:18:05 +0530

    MyIcon assignment

---
+++ MyIcon/MyIcon.cpp
@@ -0,0 +1,85 @@
+#include<Windows.h>
+#include"resource.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable declaration 
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//MessageBox(NULL, TEXT("Application is started"), TEXT("Message"), MB_OK | MB_ICONINFORMATION);
+
+	//initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;		//specialist
+	PAINTSTRUCT PS;
+	RECT RC;
+	TCHAR str[255] = TEXT("HELLO WORLD");
+
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_PAINT:
+		break;
+		// get client area changing rectangle
+		GetClientRect(hwnd, &RC);
+		//call the specialist i.e create hdc
+		hdc = BeginPaint(hwnd, &PS);
+		SetTextColor(hdc, RGB(0, 255, 0));
+		SetBkColor(hdc, RGB(0, 0, 0));
+		DrawText(hdc, str, -1, &RC, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		//send back the specialist and stop painting
+		EndPaint(hwnd, &PS);
+		//break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
